23cb3a6fdd694076bfe6710d55b7cba8f336399a
mohamedbaalsoufi <116657130+mohamedbaalsoufi@users.noreply.github.com>
2023-04-25 16:33:20 +0200

    meme

--- src/Prosjekt0X_BeskrivendeTekst/DerivationCreativeMohamed/PlotCodeDerMohamed.m
+++ src/Prosjekt0X_BeskrivendeTekst/DerivationCreativeMohamed/PlotCodeDerMohamed.m
@@ -1,23 +1,41 @@
 clear all
 
-k = 0;
+
+
+% Tid=[0:0.01:74.61];
+online= true;
+k=1;
 G = 9.81;
-h_r=80;
 h_d=61.45;
-while k<75
-    for k = 0:1:60
-        v=1.33; %rise velocity
+h_r=80;
+v(1)=0;
+
+while k<7462
+    if online
+        if k==1
+            tic
+            Tid(1) = 0;
+        else
+            Tid(k) = Tid(k-1)+0.01;
+        end
     end
-    for k = 60:1:70
-        v=0; %before drop
+    
+    if (0<=Tid(k)) && (Tid(k)<=60)
+        v(k)= -80/60;
+    elseif (60<Tid(k)) && (Tid(k)<=70)
+        v(k)=0;
+    elseif (70<Tid(k)) && (Tid(k)<=73.54)
+        v(k)=G*Tid(k)-686.7;
+    elseif (73.54<Tid(k)) && (Tid(k)<=74.62)
+        v(k)=-3.24*G*Tid(k)+2372.32;
+    else
+        v(k)=0;
     end
-    for k = 70:0.1:73.54
-        v = sqrt(2*G*h_d); % drop velocity
-    end
-    for k = 73.54:74.61:0.01
-        v = -sqrt(3.3*G*2*(h_r-h_d)); %braking velocity
-    end
+
     k=k+1;
+
 end
 
-plot(k,v)
+         
+ plot(Tid(1:k-1),v(1:k-1))
+       
